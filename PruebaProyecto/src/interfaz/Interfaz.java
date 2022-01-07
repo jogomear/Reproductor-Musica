736abd7d1c7d76d84d5379f526ad93a225d5c15f
juanibarra-a <juanibarra1001@gmail.com>
2022-01-07 21:11:30 +0100

    aaaaa
    aaaaa

--- PruebaProyecto/src/interfaz/Interfaz.java
+++ PruebaProyecto/src/interfaz/Interfaz.java
@@ -1,22 +1,61 @@
 package interfaz;
 
 
+
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.RowData;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.*;
 
+import cliente.Cliente;
+
 public class Interfaz {
+	
 	public static void main(String[] args) {
+		
+		Cliente cl = new Cliente("localhost", 8000);
 		Display d = new Display();
 		Shell s = new Shell(d);
-		s.setText("Practica SWT");
-		TabFolder tf = new TabFolder(s, SWT.TOP);
-		TabItem ti1 = new TabItem(tf, SWT.NONE);
-		TabItem ti2 = new TabItem(tf, SWT.NONE);
-		TabItem ti3 = new TabItem(tf, SWT.NONE);
-		ti1.setText("Pestaaskdjbaskfbskdbf");
-		ti2.setText("Pestaña-2");
-		ti3.setText("Pestaña-3");
-		tf.pack();
+		s.setLayout(new RowLayout(SWT.VERTICAL));
+		s.setText("TrabajoReproductorStreaming");
+		
+		//cl.getCanciones();
+		
+		final List listaDisponibles = new List(s, SWT.BORDER|SWT.V_SCROLL|SWT.SINGLE);
+		listaDisponibles.setLayoutData(new RowData(240,100));
+		
+		listaDisponibles.add("aaa");
+		listaDisponibles.add("bbb");
+		listaDisponibles.add("aaa");
+		listaDisponibles.add("bbb");
+		listaDisponibles.add("aaa");
+		listaDisponibles.add("bbb");
+		listaDisponibles.add("aaa");
+		listaDisponibles.add("bbb");
+		listaDisponibles.add("aaa");
+		listaDisponibles.add("bbb");
+		listaDisponibles.add("aaa");
+		listaDisponibles.add("bbb");
+		
+		listaDisponibles.addSelectionListener(new SelectionAdapter() {
+			
+			public void widgetSelected(SelectionEvent event) {
+				int index = listaDisponibles.getSelectionIndex();
+			}
+		});
+		
+		
+		ToolBar tb = new ToolBar(s, SWT.BOTTOM);
+		ToolItem ti1 = new ToolItem(tb, SWT.PUSH);
+		ToolItem ti2 = new ToolItem(tb, SWT.PUSH);
+		ToolItem ti3 = new ToolItem(tb, SWT.PUSH);
+		ti1.setText("Pause");
+		ti2.setText("Play");
+		ti3.setText("Siguiente");
+
+		tb.pack();
 		s.pack();
 		s.open();
 		while(!s.isDisposed()) {
@@ -27,3 +66,4 @@ public class Interfaz {
 		d.dispose();
 	}
 }
+
--- PruebaProyecto/src/interfaz/Interfazz.java
+++ PruebaProyecto/src/interfaz/Interfazz.java
@@ -1,22 +1,111 @@
 package interfaz;
 
 import org.eclipse.swt.*;
+import org.eclipse.swt.events.MenuAdapter;
+import org.eclipse.swt.events.MenuEvent;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowData;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Layout;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.swt.widgets.ToolItem;
 
 public class Interfazz {
 
 	public static void main(String[] args) {
-		Display d = new Display();
-		Shell s = new Shell(d);
-		s.setText("maestro");
-		s.pack();
-		s.open();
-		while(!s.isDisposed()) {
-			if (!d.readAndDispatch()) {
-				d.sleep();
-			}
-		}
-		d.dispose();
+		Display display = new Display();
+	    Shell shell = new Shell(display);
+	    shell.setText("List Example");
+	    shell.setSize(270, 300);
+	    shell.setLayout(new GridLayout(1, false));
+	    
+	    Group g1 = new Group(shell, SWT.NONE);
+	    g1.setText("CancionesDisponibles");
+	    g1.setLayout(new GridLayout(1, false));
+	    g1.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+	    
+	    Group g2 = new Group(shell, SWT.NONE);
+	    g2.setText("Controles");
+	    g2.setLayout(new GridLayout(1, false));
+	    g2.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+	    
+	    final List list = new List(g1, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+	    list.setLayoutData(new GridData(200,100));
+	    
+
+	    for (int loopIndex = 0; loopIndex < 100; loopIndex++) {
+	      list.add("Item " + loopIndex);
+	    }
+
+	    list.addSelectionListener(new SelectionListener() {
+
+	      public void widgetSelected(SelectionEvent event) {
+	    	  int selections = list.getSelectionIndex();
+		        String outText = "";
+		        
+		        outText += selections + " ";
+		        System.out.println("You selected: " + outText);
+	      }
+
+	      public void widgetDefaultSelected(SelectionEvent event) {
+	        int selections = list.getSelectionIndex();
+	        String outText = "";
+	        
+	        outText += selections + " ";
+	        System.out.println("You selected: " + outText);
+	      }
+	    });
+	    
+	    final Menu menu = new Menu(list);
+	    
+	    list.setMenu(menu);
+	    menu.addMenuListener(new MenuAdapter() {
+	    	
+	    	public void menuShown(MenuEvent e) {
+	    		
+	    		int index = list.getSelectionIndex();
+	    		MenuItem mi = new MenuItem(menu, SWT.PUSH);
+	    		mi.setText("Añadir a la cola");
+	    		
+	    	}
+	    });
+	    
+	    
+	    ToolBar tb = new ToolBar(g2, SWT.BOTTOM);
+	    GridLayout gl = new GridLayout();
+	    gl.numColumns = 2;
+	    gl.makeColumnsEqualWidth = true;
+	    tb.setLayout(gl);
+	    
+		ToolItem ti1 = new ToolItem(tb, SWT.PUSH);
+		ToolItem ti3 = new ToolItem(tb, SWT.PUSH);
+		
+		
+		ti1.setImage(new Image(display, "play.png"));
+//		ti2.setImage(new Image(display, "pausa.png"));
+		ti3.setImage(new Image(display, "skip.png"));
+		
+		
+		
+	    shell.open();
+	    while (!shell.isDisposed()) {
+	      if (!display.readAndDispatch())
+	        display.sleep();
+	    }
+	    display.dispose();
+	  
 	}
 }
